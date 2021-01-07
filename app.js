d5632dee20cc4c1ea43f828e988f644add3be103
phsequeira <phsequeira@yahoo.com>
2021-01-07 09:59:20 -0800

    fixed JS code

--- app.js
+++ app.js
@@ -21,21 +21,17 @@ const divideY = document.getElementById('divideY');
 
 //addition equation
 addButton.addEventListener('click', () => {
-    console.log(addX / addY)
-addAnswer.textContent = ((addX.value*1) + (addY.value*1))
-})
+    addAnswer.textContent = ((addX.value * 1) + (addY.value * 1));
+});
 //subtraction equation
 subButton.addEventListener('click', () => {
-    console.log(subX - subY)
-subAnswer.textContent = subX.value - subY.value
-})
+    subAnswer.textContent = subX.value - subY.value;
+});
 //multiplication equation
 multButton.addEventListener('click', () => {
-    console.log(multX / multY)
-multAnswer.textContent = multX.value * multY.value
-})
+    multAnswer.textContent = multX.value * multY.value;
+});
 //division equation
 divideButton.addEventListener('click', () => {
-    console.log(divideX / divideY)
-divideAnswer.textContent = divideX.value / divideY.value
-})
+    divideAnswer.textContent = divideX.value / divideY.value;
+});
